05ea3d58e95ea39dbab3c133cdcc6ba2ef927dda
Raphael BORNET--INDERSIE <borneti231@etu.unicaen.fr>
2024-10-07 14:48:42 +0200

    Squashed commit of the following:
    commit d83848c979545e4a37a515d9ddf17892c16b9dca
    Author: Raphael BORNET--INDERSIE <borneti231@etu.unicaen.fr>
    Date:   Mon Oct 7 14:42:59 2024 +0200
    
        finished v1 functionalities, only css missing
    
    commit d69d2383cc9dc125a6ae21144d3d6c76593fbe59
    Author: Raphael BORNET--INDERSIE <borneti231@etu.unicaen.fr>
    Date:   Mon Oct 7 13:54:04 2024 +0200
    
        Squashed commit of the following:
    
        commit 611b37b63a1366443c3b1f58119478ca3bd89300
        Author: Raphael BORNET--INDERSIE <borneti231@etu.unicaen.fr>
        Date:   Mon Oct 7 13:51:00 2024 +0200
    
            v1 almost done, missing weather result generation instead of direct display
    
        commit 451588f39579456b9fbd21ea98fccca2c4eb5ff1
        Author: Râphael BORNET--INDERSIE <borneti231@etu.unicaen.fr>
        Date:   Wed Oct 2 11:54:05 2024 +0200
    
            quick fix
    
        commit 445cd7b90c21cf18c69746dd743547bf0db8ae59
        Author: Râphael BORNET--INDERSIE <borneti231@etu.unicaen.fr>
        Date:   Wed Oct 2 11:41:24 2024 +0200
    
            major update, form loads cities matching the postcode once postcode is typed, gives possibility to select city option between different ones and retrieves weather data from picked city
    
        commit 0a64194f35677b1d8156bbf9dd00e3d815a80668
        Author: Râphael BORNET--INDERSIE <borneti231@etu.unicaen.fr>
        Date:   Wed Oct 2 10:25:39 2024 +0200
    
            retrieval of postcode to generate api request full url
    
    commit 2f5abc3642916d12667b2a72f5dd3da2101823f6
    Author: Râphael BORNET--INDERSIE <borneti231@etu.unicaen.fr>
    Date:   Wed Oct 2 09:49:56 2024 +0200
    
        added basic postcode form
    
    commit f460a9c4181699fac5c8a5e9bc7da786df96c0de
    Author: Gauthier Detroussel <detrous231@etu.unicaen.fr>
    Date:   Wed Oct 2 08:45:02 2024 +0200
    
        test
    
    commit 0177db6f813fa368324177dae246d63dcbbe19b5
    Author: Râphael BORNET--INDERSIE <borneti231@etu.unicaen.fr>
    Date:   Wed Oct 2 08:20:23 2024 +0200
    
        added project base structure

--- js/script.js
+++ js/script.js
@@ -10,23 +10,20 @@ let labSun = document.getElementById("dSun");
 let labRain = document.getElementById("pRain");
 
 document.addEventListener("DOMContentLoaded", function() {
-
-
-    async function fetchCities(postCode){
+    async function fetchCities(postCode) {
         let url = decoupURL + postCode;
-        try{
+        try {
             const response = await fetch(url);
             const data = await response.json();
             console.table(data);
             return data;
-        } catch(err){
+        } catch (err) {
             console.error("Une rreur est surveune lors de la requête API. Erreur :", err);
             throw err;
         }
     }
 
-
-    function displayCities(data){
+    function displayCities(data) {
         pickedCity.innerHTML = "";
         data.forEach(city => {
             const option = document.createElement("option");
@@ -39,59 +36,79 @@ document.addEventListener("DOMContentLoaded", function() {
 
     }
 
-
-    async function fetchWeatherByCity(city){
-        let url = "https://api.meteo-concept.com/api/forecast/daily/0?token="+ apitoken + "&insee=" + city;
-        try{
+    async function fetchWeatherByCity(city) {
+        let url = "https://api.meteo-concept.com/api/forecast/daily/0?token=" + apitoken + "&insee=" + city;
+        try {
             const response = await fetch(url);
             const data = await response.json();
             return data;
-        } catch(err){
+        } catch (err) {
             console.error("Une erreur est survenue lors de la requête API de récupération d'infos météo. Erreur :", err);
             throw err;
         }
     }
 
-    pickedPostCode.addEventListener("input", async function(){
+    pickedPostCode.addEventListener("input", async function () {
         let postCodeValue = pickedPostCode.value;
         pickedCity.style.display = "none";
         submit.style.display = "none";
-
-        if (/^\d{5}$/.test(postCodeValue)){
-            try{
+        if (/^\d{5}$/.test(postCodeValue)) {
+            try {
                 let data = await fetchCities(postCodeValue);
                 displayCities(data);
-            } catch(err){
+            } catch (err) {
                 console.error("Une erreur est survenue lors de la recherche de commune. Erreur :", err);
                 throw err;
             }
         }
     });
 
-
-
-    submit.addEventListener("click", async function(){
-       let pickedCityValue = pickedCity.value;
-       console.log(pickedCityValue);
-       if (pickedCityValue != null){
-            try{
+    submit.addEventListener("click", async function () {
+        let pickedCityValue = pickedCity.value;
+        console.log(pickedCityValue);
+        if (pickedCityValue != null) {
+            try {
                 let data = await fetchWeatherByCity(pickedCityValue);
-                displayWeather(data);
+                createWeatherCard(data);
                 console.table(data);
-            } catch (err){
+            } catch (err) {
                 console.error("Une erreur est survenue lors de la recherche d'informations météo. Erreur :", err);
                 throw err;
             }
         }
     });
 
+    async function createWeatherCard(data) {
+        let tMinCard = document.createElement("div");
+        let tMaxCard = document.createElement("div");
+        let sunCard = document.createElement("div");
+        let rainCard = document.createElement("div");
 
-    async function displayWeather(data){
-        labCity.textContent = data["city"]["name"];
-        labTmin.textContent = data["forecast"]["tmin"];
-        labTmax.textContent = data["forecast"]["tmax"];
-        labSun.textContent = data["forecast"]["sun_hours"];
-        labRain.textContent = data["forecast"]["probarain"];
+        tMinCard.textContent = `Température minimale : ${data["forecast"]["tmin"]}°C`;
+        tMaxCard.textContent = `Température maximale : ${data["forecast"]["tmax"]}°C`;
+        sunCard.textContent = `Ensoleillement : ${data["forecast"]["sun_hours"]}h`;
+        rainCard.textContent = `Probabilité de pluie : ${data["forecast"]["probarain"]}%`;
+
+        let request = document.getElementById("request");
+        let weather = document.getElementById("weather");
+
+        weather.appendChild(tMinCard);
+        weather.appendChild(tMaxCard);
+        weather.appendChild(sunCard);
+        weather.appendChild(rainCard);
+
+        let newSearchButton = document.createElement("div");
+        newSearchButton.textContent = "Nouvelle recherche";
+        newSearchButton.classList.add("newSearchButton");
+        document.body.appendChild(newSearchButton);
+
+        newSearchButton.addEventListener("click", function () {
+            window.location.reload();
+        });
+
+
+        request.style.display = "none";
+        weather.style.display = "block";
     }
 
 
